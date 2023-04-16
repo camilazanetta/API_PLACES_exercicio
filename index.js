46749927d449975ce16174ee6887f87cca6d3c59
camilazanetta <camilazanetta@hotmail.com>
2023-04-16 04:10:17 -0300

    third commit

--- index.js
+++ index.js
@@ -4,8 +4,31 @@ const Place = require('./src/models/place')
 
 const app = express()
 
+app.use(express.json()) // obrigatório! //
+
 connection.authenticate()
 connection.sync({alter: true})
+
+app.post('/places', async (request, response) => {
+    try {
+        const data = {
+            name: request.body.name,
+            contact: request.body.contact,
+            opening_hours: request.body.opening_hours,
+            description: request.body.description,
+            latitude: request.body.latitude,
+            longitude: request.body.longitude,
+        }
+
+        const place = await Place.create (data)
+
+        response.status(201).json(place)
+
+    } catch (error) {
+        response.status(500).json({message: 'Não foi possível concluir a operação'})
+    }
+
+})
 
 app.listen (8888, () => {  //subi em porta diferente para não dar conflito//
     console.log ("Servidor online")
